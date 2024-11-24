cc529fbc05048835616f54dbe3313b60453f01d4
Arco Mul <me@arcomul.nl>
2024-11-24 21:36:26 +0100

    fix: require statement in reservation.cron.pb.js

--- pocketbase/pb_hooks/reservation.cron.pb.js
+++ pocketbase/pb_hooks/reservation.cron.pb.js
@@ -4,7 +4,7 @@
 // 0 12 * * * - every day at 12:00
 cronAdd("reservation-reminder", "0 12 * * *", () => {
   /** @type {typeof import('./lib/location')} */
-  const { sendReminders } = require(`${__hooks}./lib/location`);
+  const { sendReminders } = require(`${__hooks}/lib/location`);
   console.log(`[cron/reservation-reminder] start`);
 
   // Get active locations
