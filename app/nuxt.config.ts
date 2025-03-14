05974fdb03b5c3822287a7720d025c5d8cf7da17
Arco Mul <me@arcomul.nl>
2025-03-14 15:52:49 +0100

    chore: update dependencies

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  compatibilityDate: "2025-03-14",
   runtimeConfig: {
     public: {
       plausibleTrackingDomain: "", // NUXT_PUBLIC_PLAUSIBLE_TRACKING_DOMAIN
